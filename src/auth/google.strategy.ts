1ee185854558127ac3eab7d5058aa8650ff669db
Borko Rajkovic <rajkovicborko@gmail.com>
2019-09-14 00:44:21 +0200

    Validation of config

--- src/auth/google.strategy.ts
+++ src/auth/google.strategy.ts
@@ -7,13 +7,12 @@ import { ConfigService } from '../config/config.service';
 export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
   constructor(config: ConfigService) {
     super({
-      clientID: config.get('clientID'),
-      clientSecret: config.get('clientSecret'),
+      clientID: config.clientId,
+      clientSecret: config.clientSecret,
       callbackURL: 'http://localhost:3000/auth/google/callback',
       passReqToCallback: true,
       scope: ['profile'],
     });
-    console.log('Test', config.get('clientID'), config.get('clientSecret'));
   }
 
   async validate(
@@ -24,8 +23,6 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
     done: any,
   ) {
     try {
-      console.log(profile);
-
       const jwt: string = 'placeholderJWT';
       const user = {
         jwt,
--- src/config/config.service.ts
+++ src/config/config.service.ts
@@ -1,14 +1,47 @@
+import * as Joi from '@hapi/joi';
 import * as dotenv from 'dotenv';
 import * as fs from 'fs';
 
+export interface EnvConfig {
+  [key: string]: string;
+}
+
 export class ConfigService {
-  private readonly envConfig: { [key: string]: string };
+  private readonly envConfig: EnvConfig;
 
   constructor(filePath: string) {
-    this.envConfig = dotenv.parse(fs.readFileSync(filePath));
+    const config = dotenv.parse(fs.readFileSync(filePath));
+    this.envConfig = this.validateInput(config);
   }
 
-  get(key: string): string {
-    return this.envConfig[key];
+  get clientId(): string {
+    return this.envConfig.CLIENT_ID;
+  }
+
+  get clientSecret(): string {
+    return this.envConfig.CLIENT_SECRET;
+  }
+
+  /**
+   * Ensures all needed variables are set, and returns the validated JavaScript object
+   * including the applied default values.
+   */
+  private validateInput(envConfig: EnvConfig): EnvConfig {
+    const envVarsSchema: Joi.ObjectSchema = Joi.object({
+      NODE_ENV: Joi.string()
+        .valid('development', 'production', 'test', 'provision')
+        .default('development'),
+      PORT: Joi.number().default(3000),
+      CLIENT_ID: Joi.string().required(),
+      CLIENT_SECRET: Joi.string().required(),
+    });
+
+    const { error, value: validatedEnvConfig } = envVarsSchema.validate(
+      envConfig,
+    );
+    if (error) {
+      throw new Error(`Config validation error: ${error.message}`);
+    }
+    return validatedEnvConfig;
   }
 }
